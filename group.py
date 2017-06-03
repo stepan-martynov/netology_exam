e83f594db073bbb8f65b2d4a31c38ab4ea862b7f
Stepan Martynov <smartd88@gmail.com>
2017-06-03 23:38:49 +0300

    Снова п1: теперь и execute выполняется через call_api()
    п2 оставил как комментарий, чтобы всегда можно было сгенерировать token
    
    п3 сделал
    
    п4 исправил как Вы предложиди. На тот момент как писал впервые не понимал как сделать не компрехеншном
    
    п5,6 исправил

--- group.py
+++ group.py
@@ -2,7 +2,6 @@ from pprint import pprint
 import sys
 import time
 from urllib.parse import urlencode
-
 import requests
 import json
 
@@ -23,42 +22,25 @@ import json
 #
 # print('?'.join((AOUTH_URL, urlencode(auth_data))))
 
-def simple_request_to_api(req_url, params):
-    resp = requests.get(req_url, params).json()
+def call_api(method, params):
+    url_base = 'https://api.vk.com/method/'
+
+    resp = requests.get(''.join([url_base, method]), params).json()
     if 'error' in resp.keys():
         with open('api_error.log', 'a') as f:
             f.write(
                 '{}: {} // {}\n'.format(time.ctime(time.time()), resp['error']['error_msg'], ' '.join(params.values())))
         time.sleep(0.4)
-        simple_request_to_api(req_url, params, **code_str)
+        # call_api(req_url, params, **code_str)
     resp = resp['response']
     return resp
 
 
-def get_list_friends_groups(friends_list, access_token):
-    req_link = 'https://api.vk.com/method/execute'
-
-    req_code = ',\n'.join(['"%d": API.groups.get({"user_id": %d})' % (i, i) for i in friends_list])
-    req_code = 'return {%s};' % req_code
-    req_data = {
-        'code': req_code,
-        'access_token': access_token
-    }
-
-    response = requests.get(req_link, req_data)
-
-    friends_groups_dict = response.json()['response']
-    friends_groups_list = set()
-
-    for i in friends_groups_dict:
-        if friends_groups_dict[i]:
-            friends_groups_list = friends_groups_list | set(friends_groups_dict[i])
-
-    return friends_groups_list
-
-
-def creat_list_of_friends_list(friends_list):
-    list_of_friends_lists = list(friends_list[(i * 25):(i * 25 + 25)] for i in range(len(friends_list) // 25 + 1))
+def create_list_of_friends_list(friends_list):
+    list_of_friends_lists = list()
+    step = 25
+    for i in range(0, len(friends_list), step):
+        list_of_friends_lists.append(friends_list[i: i + step])
     return list_of_friends_lists
 
 
@@ -71,39 +53,54 @@ def term_print_dot(all_groups, i, unique_groups_list):
 def main():
     with open('api_error.log', 'w') as f:
         f.write('Started at {}\n'.format(time.ctime(time.time())))
-    access_token = input('Введите access token: ')
+
     user_id = input('Введите id пользователя, за которым будем следить: ')
+    access_token = input('Введите access token: ')
+
     pprint(time.ctime(time.time()))
     try:
         user_id = int(user_id)
     except ValueError:
-        req_url = 'https://api.vk.com/method/users.get'
+        method = 'users.get'
         params = {
             'user_ids': user_id,
             'access_token': access_token
         }
-        user_id = simple_request_to_api(req_url, params)[0]['uid']
+        user_id = call_api(method, params)[0]['uid']
 
     params = {
         'user_id': user_id,
         'access_token': access_token
     }
-    req_url = 'https://api.vk.com/method/friends.get'
-    friends_list = simple_request_to_api(req_url, params)
+    method = 'friends.get'
+    friends_list = call_api(method, params)
 
-    list_of_friends_lists = creat_list_of_friends_list(friends_list)
+    list_of_friends_lists = create_list_of_friends_list(friends_list)
 
     params = {
         'user_id': user_id,
         'access_token': access_token
     }
-    req_url = 'https://api.vk.com/method/groups.get'
+    method = 'groups.get'
 
-    groups_ids = set(simple_request_to_api(req_url, params))
+    groups_ids = set(call_api(method, params))
     all_groups = len(groups_ids)
 
     for i, short_friends_list in enumerate(list_of_friends_lists):
-        friends_groups_list = get_list_friends_groups(short_friends_list, access_token)
+
+        method = 'execute'
+        req_code = ',\n'.join(['"{}": API.groups.get({{"user_id": {}}})'.format(i, i) for i in short_friends_list])
+        req_code = 'return {%s};' % req_code
+        params = {
+            'code': req_code,
+            'access_token': access_token
+        }
+        friends_groups_dict = call_api(method, params)
+
+        friends_groups_list = set()
+        for uid in friends_groups_dict:
+            if friends_groups_dict[uid]:
+                friends_groups_list = friends_groups_list | set(friends_groups_dict[uid])
         groups_ids = groups_ids.difference(friends_groups_list)
         groups_list_len = len(groups_ids)
         term_print_dot(all_groups, i, groups_list_len)
@@ -111,18 +108,18 @@ def main():
     print()
     print('Формируем расширенный список уникальных групп')
 
-    req_url = 'https://api.vk.com/method/groups.getById'
+    method = 'groups.getById'
 
     params = {
         'group_ids': ','.join(map(str, groups_ids)),
         'fields': 'members_count',
         'access_token': access_token
     }
-    groups_info = simple_request_to_api(req_url, params)
+    groups_info = call_api(method, params)
 
     print('Записываем в файл')
     with open('groups_info.json', 'w', encoding='utf-8') as f:
-        f.write('Writed at {}. {} unque groups\n'.format(time.ctime(time.time()), len(groups_info)))
+        f.write('Writed at {}. {} unque groups.\n'.format(time.ctime(time.time()), len(groups_info)))
         json.dump(groups_info, f, indent=2, ensure_ascii=False)
     print('Файлы находятся в файле groups_info.json')
     pprint(time.ctime(time.time()))
