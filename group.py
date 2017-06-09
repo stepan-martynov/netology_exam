71290ee625c21053ad386b8118660c77c12b4e43
Stepan Martynov <smartd88@gmail.com>
2017-06-09 22:06:56 +0300

    Убрал я прерывыае. Будут ошибки большого числа запросов, отпишится в лог. Убрал запись ошибок в файл, чтобы вообще не держать файл открытым. Создал класс CallApi. Разобрался, немного с этой темой. Вроде ничего сложного, но возможности интересные.

--- group.py
+++ group.py
@@ -22,23 +22,62 @@ import json
 #
 # print('?'.join((AOUTH_URL, urlencode(auth_data))))
 
-def call_api(method, params):
-    url_base = 'https://api.vk.com/method/'
-    params['V'] = 5.65
+class CallApi:
+    def __init__(self, method, params):
+        self.method = method
+        self.params = params
+        self.params['V'] = 5.65
+        self.url_base = 'https://api.vk.com/method/'
 
-    resp = requests.get(''.join([url_base, method]), params).json()
-    if 'error' in resp.keys():
-        with open('api_error.log', 'a') as f:
-            f.write(
-                '{}: {} // {}\n'.format(time.ctime(time.time()), resp['error']['error_msg'], ' '.join(params.values())))
-        time.sleep(0.4)
-        # call_api(req_url, params)
-    # pprint(resp)
-    resp = resp['response']
-    return resp
+    def __str__(self):
+        print(self.method)
+
+    def response(self):
+        response = requests.get(''.join([self.url_base, self.method]), self.params).json()
+        if 'error' in response.keys():
+            print('\n{}: {} // {}'.format(time.ctime(time.time()), response['error']['error_msg'], self.method))
+        return response['response']
 
 
-def create_list_of_friends_list(friends_list):
+def get_user_id(user_id, access_token):
+    try:
+        user_id = int(user_id)
+    except ValueError:
+        method = 'users.get'
+        params = {
+            'user_ids': user_id,
+            'access_token': access_token
+        }
+        user_id = CallApi(method, params).response()[0]['uid']
+    return user_id
+
+
+def get_user_groups(user_id, access_token):
+    params = {
+        'user_id': user_id,
+        'access_token': access_token
+    }
+    method = 'groups.get'
+
+    return set(CallApi(method, params).response())
+
+
+def get_user_friends_list(user_id, access_token):
+    params = {
+        'user_id': user_id,
+        'access_token': access_token
+    }
+    method = 'friends.get'
+    return CallApi(method, params).response()
+
+
+def create_list_of_friends_list(user_id, access_token):
+    params = {
+        'user_id': user_id,
+        'access_token': access_token
+    }
+    method = 'friends.get'
+    friends_list = CallApi(method, params).response()
     list_of_friends_lists = list()
     step = 25
     for i in range(0, len(friends_list), step):
@@ -52,61 +91,7 @@ def term_print_dot(all_groups, i, unique_groups_list):
     sys.stdout.write("\rОсталось {} уникальных групп из {}. /{}/".format(unique_groups_list, all_groups, dots + spaces))
 
 
-def main():
-    with open('api_error.log', 'w') as f:
-        f.write('Started at {}\n'.format(time.ctime(time.time())))
-
-    user_id = input('Введите id пользователя, за которым будем следить: ')
-    access_token = input('Введите access token: ')
-
-    pprint(time.ctime(time.time()))
-    try:
-        user_id = int(user_id)
-    except ValueError:
-        method = 'users.get'
-        params = {
-            'user_ids': user_id,
-            'access_token': access_token
-        }
-        user_id = call_api(method, params)[0]['uid']
-
-    params = {
-        'user_id': user_id,
-        'access_token': access_token
-    }
-    method = 'friends.get'
-    friends_list = call_api(method, params)
-
-    list_of_friends_lists = create_list_of_friends_list(friends_list)
-
-    params = {
-        'user_id': user_id,
-        'access_token': access_token
-    }
-    method = 'groups.get'
-
-    groups_ids = set(call_api(method, params))
-    all_groups = len(groups_ids)
-
-    for i, short_friends_list in enumerate(list_of_friends_lists):
-
-        method = 'execute'
-        req_code = ',\n'.join(['"{}": API.groups.get({{"user_id": {}}})'.format(i, i) for i in short_friends_list])
-        req_code = 'return {%s};' % req_code
-        params = {
-            'code': req_code,
-            'access_token': access_token
-        }
-        friends_groups_dict = call_api(method, params)
-
-        friends_groups_list = set()
-        for uid in friends_groups_dict:
-            if friends_groups_dict[uid]:
-                friends_groups_list = friends_groups_list | set(friends_groups_dict[uid])
-        groups_ids = groups_ids.difference(friends_groups_list)
-        groups_list_len = len(groups_ids)
-        term_print_dot(all_groups, i, groups_list_len)
-        time.sleep(0.3)
+def get_groups_info(access_token, groups_ids):
     print()
     print('Формируем расширенный список уникальных групп')
 
@@ -117,13 +102,54 @@ def main():
         'fields': 'members_count',
         'access_token': access_token
     }
-    groups_info = call_api(method, params)
+    groups_info = CallApi(method, params).response()
 
     print('Записываем в файл')
     with open('groups_info.json', 'w', encoding='utf-8') as f:
-        f.write('Writed at {}. {} unque groups.\n'.format(time.ctime(time.time()), len(groups_info)))
+        f.write('//Writed at {}. {} unque groups.\n'.format(time.ctime(time.time()), len(groups_info)))
         json.dump(groups_info, f, indent=2, ensure_ascii=False)
     print('Файлы находятся в файле groups_info.json')
+
+
+def get_and_filter_groups_set(list_of_friends_lists, all_groups, groups_ids, access_token):
+    for i, short_friends_list in enumerate(list_of_friends_lists):
+
+        method = 'execute'
+        req_code = ',\n'.join(['"{}": API.groups.get({{"user_id": {}}})'.format(i, i) for i in short_friends_list])
+        req_code = 'return {%s};' % req_code
+        params = {
+            'code': req_code,
+            'access_token': access_token
+        }
+        friends_groups_dict = CallApi(method, params).response()
+
+        friends_groups_list = set()
+        for uid in friends_groups_dict:
+            if friends_groups_dict[uid]:
+                friends_groups_list = friends_groups_list | set(friends_groups_dict[uid])
+        groups_ids = groups_ids.difference(friends_groups_list)
+        groups_list_len = len(groups_ids)
+        term_print_dot(all_groups, i, groups_list_len)
+    return groups_ids
+
+
+def main():
+    user_id = input('Введите id пользователя, за которым будем следить: ')
+    access_token = input('Введите access token: ')
+
+    pprint(time.ctime(time.time()))
+
+    user_id = get_user_id(user_id, access_token)
+
+    groups_ids = get_user_groups(user_id, access_token)
+
+    list_of_friends_lists = create_list_of_friends_list(user_id, access_token)
+
+    all_groups = len(groups_ids)
+
+    groups_ids = get_and_filter_groups_set(list_of_friends_lists, all_groups, groups_ids, access_token)
+
+    get_groups_info(access_token, groups_ids)
     pprint(time.ctime(time.time()))
 
 
